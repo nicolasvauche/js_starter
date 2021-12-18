e5101dc63f6e3cb1d45f294cbaa2bf476e7bfd9f
Nicolas vauché <hello@nicolasvauche.net>
2021-12-18 16:32:27 +0100

    End

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
+/* ************************************ */
 /* PARTIE 1 : Déclaration des variables */
+/* ************************************ */
 
 // On pointe sur le bouton
 const button = document.getElementById('submit')
 
-// On récupère le prénom saisi dans l'input
+// On pointe sur le champ de formulaire
 const prenom = document.getElementById('prenom')
 
 // On pointe sur le paragraphe
 const paragraphe = document.getElementById('texte')
 
+/* ************************************ */
 /* PARTIE 2 : Déclaration des fonctions */
+/* ************************************ */
 
 // On déclare la fonction qui écrira dans le paragraphe
 const ecrirePrenom = () => {
-  // On modifie le contenu du paragraphe
-  paragraphe.innerHTML = prenom.value
+  // On débug notre champ dans la console
+  console.log(prenom.value)
+
+  // Seulement si le champ n'est pas vide
+  if (prenom.value !== '') {
+    // On modifie le contenu du paragraphe
+    paragraphe.innerHTML = 'Bonjour ' + prenom.value + ' !'
+  }
 }
 
+/* ******************** */
 /* PARTIE 3 : Programme */
+/* ******************** */
 
 // On ajoute un écouteur d'éveènement sur le click du bouton
 button.addEventListener('click', ecrirePrenom)
